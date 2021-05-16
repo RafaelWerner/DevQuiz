f1c8c9637f03b9a44623df295443ebc9417035e9
Rafael Gustavo Simon Werner <rafaelgustavowerner@gmail.com>
2021-05-16 20:21:06 -0300

    Corrige espaçamentos

--- lib/home/widgets/quiz_card/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card/quiz_card_widget.dart
@@ -33,19 +33,19 @@ class QuizCardWidget extends StatelessWidget {
             style: AppTextStyles.heading15,
           ),
           SizedBox(
-            height: 24,
+            height: 20,
           ),
           Row(
             children: [
               Expanded(
-                flex: 1,
+                flex: 2,
                 child: Text(
                   "3 de 10",
                   style: AppTextStyles.body11,
                 ),
               ),
               Expanded(
-                flex: 4,
+                flex: 3,
                 child: LinearProgressIndicator(
                   value: .3,
                   backgroundColor: AppColors.chartSecondary,

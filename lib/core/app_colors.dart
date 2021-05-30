c6c1f470c1672dd06858723c22b86435761a1a9e
Rafael Gustavo Simon Werner <rafaelgustavowerner@gmail.com>
2021-05-30 16:34:34 -0300

    Adiciona color shade

--- lib/core/app_colors.dart
+++ lib/core/app_colors.dart
@@ -5,6 +5,7 @@ class AppColors {
   static final Color white = Color(0xFFFFFFFF);
   static final Color black = Color(0xFF514766);
   static final Color grey = Color(0xFF6E6680);
+  static final Color shade = Color(0x26000000);
   static final Color lightGrey = Color(0xFFA6A1B2);
   static final Color border = Color(0xFFE1E1E6);
   static final Color chartSecondary = Color(0xFFE1E6E3);
@@ -13,7 +14,6 @@ class AppColors {
   //Greens
   static final Color lightGreen = Color(0xFFE1F5EC);
   static final Color green = Color(0xFFB8DBCB);
-  static final Color greenTransp = Color(0xCCB8DBCB);
   static final Color darkGreen = Color(0xFF04D361);
 
   //Reds
